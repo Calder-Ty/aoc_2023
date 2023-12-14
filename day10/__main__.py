20fc884de3a0a1cb664d4dc6e27fc94caf72d54d
Tyler Calder <tyler.calder@advancedmd.com>
2023-12-13 19:32:23 -0700

    feat: Finish day 10 part 2
    To solve part two I took the pipe map (an ordered array of segments)
    built by part one, and traveled it. Keeping track of every left
    and right turn I was able to determine if i was going Clockwise or
    Counter Clockwise around the loop. At each section of pipe I keep
    track of what tiles are to the left and right for the pipe (inculding
    before and after turns). If the loop is Clockwise then Right side
    tiles are inside, and for Counter clockwise left side tiles are inside
    
    Finally, I then took all my inside tiles, and for every tile, I checked
    if its neighbors were a Part of the pipe, or already accounted for
    If not I added it as an inside tile, and then did this again for that
    tile. This way I was able to fill in entire contained areas.

--- day10/__main__.py
+++ day10/__main__.py
@@ -2,6 +2,7 @@ import dataclasses
 import math
 import pathlib
 import typing
+import io
 
 import utils
 
@@ -96,11 +97,12 @@ class Coord(typing.NamedTuple):
 
     def heading(self, from_coord: Coord_T):
         """Give the direction traveled from a coord to the current"""
+        # Remember 0, 0 is TOP LEFT
         match(from_coord.x - self.x, from_coord.y - self.y):
             case(1, 0):
-                return EAST
-            case(-1, 0):
                 return WEST
+            case(-1, 0):
+                return EAST
             case(0, 1):
                 return NORTH
             case(0, -1):
@@ -108,29 +110,29 @@ class Coord(typing.NamedTuple):
             case _:
                 assert False, "Heading must be given to adjacent coords"
 
-    def get_left(self, heading: str) -> Coord_T:
+    def get_left(self, heading: str) -> typing.Optional[Coord_T]:
         match heading:
-            case NORTH:
-                self.west()
-            case EAST:
-                self.north()
-            case SOUTH:
-                self.east()
-            case WEST:
-                self.south()
+            case "north":
+                return self.west()
+            case "east":
+                return self.north()
+            case "south":
+                return self.east()
+            case "west":
+                return self.south()
             case _:
                 raise ValueError(f"Invalid heading {heading}")
 
-    def get_left(self, heading: str) -> Coord_T:
+    def get_right(self, heading: str) -> typing.Optional[Coord_T]:
         match heading:
-            case SOUTH:
-                self.west()
-            case WEST:
-                self.north()
-            case NORTH:
-                self.east()
-            case EAST:
-                self.south()
+            case "south":
+                return self.west()
+            case "west":
+                return self.north()
+            case "north":
+                return self.east()
+            case "east":
+                return self.south()
             case _:
                 raise ValueError(f"Invalid heading {heading}")
 
@@ -150,6 +152,9 @@ class Coord(typing.NamedTuple):
         # 0, 0 is in the top left corner
         return Coord(self.x+1, self.y)
 
+    def neighbors(self) -> typing.List[Coord_T]:
+        return [self.north(), self.west(), self.south(), self.east()]
+
 
 def find_start(data: typing.List[str]):
     for y, row in enumerate(data):
@@ -160,9 +165,50 @@ def find_start(data: typing.List[str]):
 
 def do_challenge2(data: typing.List[str]) -> int:
     pipe_map = build_pipe_map(data)
-    lefts = []
-    rights = []
-    degree
+    walker = PipeWalker(data, pipe_map)
+    walker.walk()
+    if walker.degree == 360:
+        # Counterclockwise
+        inside = walker.lefts
+    elif walker.degree == -360:
+        # Clockwise
+        inside = walker.rights
+    else:
+        assert False, "Mistake made calculating turns"
+
+    # now that we have the adjacent tiles that are in, but are not touching the inside
+    others = set()
+    for coord in inside:
+        to_check = {coord}
+        while to_check:
+            c = to_check.pop()
+            contained_neighbors = set(c.neighbors()).difference(others, inside, set(pipe_map))
+            # breakpoint()
+            others = others | contained_neighbors
+            to_check = to_check | (contained_neighbors)
+    print(others)
+
+    with (DATA_DIR/"output.txt").open('w') as fi:
+        for y in range(len(data)):
+            line = io.StringIO()
+            for x in range(len(data[0])):
+                coord = Coord(x, y)
+                if coord in pipe_map:
+                    line.write("█")
+                elif coord in inside:
+                    line.write("I")
+                elif coord in others:
+                    line.write("O")
+                else:
+                    line.write(" ")
+            line.write('\n')
+            line.seek(0)
+
+            fi.write(line.read())
+
+
+
+    return len(inside) + len(others)
 
 
 class PipeWalker():
@@ -176,12 +222,32 @@ class PipeWalker():
         self.degree = 0
 
     def walk(self):
-        prev = pipe_map[0]
-        for coord in pipe_map[1:]:
+        # The last coord in the previous coord in the loop
+        prev = self.pipe_map[-1]
+        for coord in self.pipe_map:
             # Get Admin Data
             heading = coord.heading(prev)
             symbol = coord_to_symbol(self.data, coord)
-            self.check_left_right(heading)
+            if symbol == "S":
+                # Replace the S with the approprate symbol
+                match coord.heading(prev), self.pipe_map[1].heading(coord):
+                    case("east", "east") | ("west", "west"):
+                        symbol = '-'
+                    case("north", "north") | ("south", "south"):
+                        symbol = '|'
+                    case("east", "north") | ("south", "west"):
+                        symbol = 'J'
+                    case("east", "south") | ("north", "west"):
+                        symbol = '7'
+                    case("west", "north") | ("south", "east"):
+                        symbol = 'L'
+                    case("west", "south") | ("north", "east"):
+                        symbol = 'F'
+                    case _:
+                        ValueError("Unreachable")
+
+            self.add_left(heading, coord)
+            self.add_right(heading, coord)
 
             # Now, handle turning:
             match symbol:
@@ -192,60 +258,101 @@ class PipeWalker():
                     # a horizontal pipe connecting east and west.
                     pass
                 case 'L':
+                    # print(coord)
                     # a 90-degree bend connecting north and east.
                     heading = NORTH if heading is WEST else EAST
                     if heading == NORTH:
                         self.degree -= 90
+                        # print(f"{symbol}: heading {heading}, turned RIGHT, degrees is {self.degree}")
                     else:
                         self.degree += 90
+                        # print(f"{symbol}: heading {heading}, turned LEFT, degrees is {self.degree}")
                 case 'J':
                     # a 90-degree bend connecting north and west.
+                    # print(coord)
                     heading = NORTH if heading is EAST else WEST
                     if heading == NORTH:
                         self.degree += 90
+                        # print(f"{symbol}: heading {heading}, turned LEFT, degrees is {self.degree}")
                     else:
                         self.degree -= 90
+                        # print(f"{symbol}: heading {heading}, turned RIGHT, degrees is {self.degree}")
                 case '7':
                     # a 90-degree bend connecting south and west.
+                    # print(coord)
                     heading = SOUTH if heading is EAST else WEST
+                    if heading == SOUTH:
+                        self.degree -= 90
+                        # print(f"{symbol}: heading {heading}, turned RIGHT, degrees is {self.degree}")
+                    else:
+                        self.degree += 90
+                        # print(f"{symbol}: heading {heading}, turned LEFT, degrees is {self.degree}")
                 case 'F':
                     # a 90-degree bend connecting south and east.
+                    # print(coord)
                     heading = SOUTH if heading is WEST else EAST
+                    if heading == SOUTH:
+                        self.degree += 90
+                        # print(f"{symbol}: heading {heading}, turned LEFT, degrees is {self.degree}")
+                    else:
+                        self.degree -= 90
+                        # print(f"{symbol}: heading {heading}, turned RIGHT, degrees is {self.degree}")
                 case _:
-                    assert False, "Unreachable"
+                    assert False, f"Unreachable, got symbol: {symbol}"
+            self.add_left(heading, coord)
+            self.add_right(heading, coord)
+            prev = coord
 
-            self.check_left_right(heading)
+    def add_left(self, heading: str, coord: Coord):
+        if left := coord.get_left(heading):
+            if left not in self.pipe_map:
+                self.lefts.add(left)
 
-    def check_left_right(self, heading: str):
-        left = coord_to_symbol(self.data, coord.get_left(heading))
-        right = coord_to_symbol(self.data, coord.get_right(heading))
-
-        if left = ".":
-            self.lefts.add(left)
-        if right = ".":
-            self.rights.add(right)
+    def add_right(self, heading: str, coord: Coord):
+        if right := coord.get_right(heading):
+            if right not in self.pipe_map:
+                self.rights.add(right)
 
 
 def coord_to_symbol(data: typing.List[str], coord: Coord):
-    return data[coord.y][coord.x]
+    if coord.y >= 0 and coord.y < len(data) and coord.x >=0 and coord.x < len(data[0]):
+        return data[coord.y][coord.x]
 
 
 def build_pipe_map(data):
     """Builds out Map of pipes"""
-    max_rows = len(data)
-    max_cols = len(data[0])
     # First Build the map
     start = find_start(data)
     # Then Do A search from each direction till we find out what to do
-    for dir in [start.north(), start.west(), start.south(), start.east()]:
+    for (coord, dir) in [(start.north(), NORTH), (start.west(), WEST), (start.south(), SOUTH), (start.east(), EAST)]:
         # Do validation
-        path = find_pipe_path(dir)
+        if coord is None:
+            continue
+        if (
+            dir == NORTH and
+            coord_to_symbol(data, coord) in ("L", "J", "-", ".")
+        ) or (
+            dir == SOUTH and
+            coord_to_symbol(data, coord) in ("7", "F", "-", ".")
+        ) or (
+            dir == WEST and
+            coord_to_symbol(data, coord) in ("7", "J", "|", ".")
+        ) or (
+            dir == EAST and
+            coord_to_symbol(data, coord) in ("F", "L", "|", ".")
+        ):
+            # Invalid Symbol to move from start
+            continue
+
+        path = find_pipe_path(start, coord, data)
         if path:
             break
     return path
 
 
-def find_pipe_path(dir) -> typing.List[Coord]:
+def find_pipe_path(start, dir, data) -> typing.List[Coord]:
+    max_rows = len(data)
+    max_cols = len(data[0])
     current = dir
     path = [start]
     while current:
